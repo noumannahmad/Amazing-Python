e419e53046fd8afcb0676043c9daef088af21b25
Nouman Ahmad <noumanahmad3777@gmail.com>
2020-05-11 23:37:43 +0500

    Word_to_PDF_File_Convert_using_python.py

---
+++ Word_to_PDF_File_Convert_using_python.py
@@ -0,0 +1,5 @@
+#install package pip install docx2pdf
+#import liabrary
+from docx2pdf import  convert
+
+convert(r"C:\Users\Nouman Ahmad\Desktop\world_file.docx",r"C:\Users\Nouman Ahmad\Desktop\pdf_converted_file.pdf")
